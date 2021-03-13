7d4ea0c92dfd6d070b8e81a5352c558fb5c2358d
Svetlana Tyshkevich <panfilova.ss@gmail.com>
2021-03-13 15:25:32 +0200

    feat: add styles for slider

--- src/components/AttractionsGallery.tsx
+++ src/components/AttractionsGallery.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  Box,
   Container,
   Grid,
   makeStyles,
@@ -20,33 +19,44 @@ interface IGalleryItemProps {
 
 const useStyles = makeStyles((theme: Theme) => ({
   wrap: {
-    height: '100vh',
+    height: '90vh',
     marginTop: 100,
     marginBottom: 200,
+    padding: 20,
     color: theme.palette.primary.contrastText,
-    boxShadow: `inset 0px -500px 370px -100px ${theme.palette.primary.main}`,
-    '@media(max-width: 514px)': {
-      height: 'auto',
-    },
   },
   font: {
     fontFamily: "'Caveat', cursive",
     color: theme.palette.secondary.main,
   },
+  gallery: {
+    '& .image-gallery-icon:hover': { color: theme.palette.secondary.light },
+    '& .image-gallery-thumbnail.active, .image-gallery-thumbnail:hover, .image-gallery-thumbnail:focus': {
+      borderColor: theme.palette.secondary.light,
+    },
+    '& .image-gallery-image': {
+      width: '100%',
+      height: 'calc(90vh - 80px)',
+      objectFit: 'cover',
+    },
+  },
 }));
 
-const AttractionsGallery: React.FunctionComponent<IGalleryProps> = ({ places
+const AttractionsGallery: React.FunctionComponent<IGalleryProps> = ({
+  places,
 }) => {
   const classes = useStyles();
   const images = places.map((item: IGalleryItemProps) => ({
+    originalTitle: item.name,
     original: `${item.photoUrl}`,
     thumbnail: `${item.photoUrl}`,
+    description: item.description,
   }));
 
   return (
     <Grid className={classes.wrap} container alignItems="flex-end">
       <Container>
-        <ImageGallery items={images} />
+        <ImageGallery items={images} additionalClass={classes.gallery} />
       </Container>
     </Grid>
   );
