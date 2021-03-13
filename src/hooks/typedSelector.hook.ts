5aa4b618925374b085d7c38ceea2f5f74fd04158
Efimovich Evgenii <53760291+johnneon@users.noreply.github.com>
2021-03-13 12:13:10 +0600

    Revert "Video"

--- src/hooks/typedSelector.hook.ts
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useSelector } from "react-redux";
-import { IRootState } from "../store/redusers";
-
-
-export const useTypedSelector: TypedUseSelectorHook<IRootState> = useSelector;
