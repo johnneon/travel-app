f892e2341ae3297137851f6a421d1217db62eacc
Svetlana Tyshkevich <panfilova.ss@gmail.com>
2021-03-14 14:54:42 +0200

    feat: add slide's name and fix styles

--- src/components/AttractionsGallery.tsx
+++ src/components/AttractionsGallery.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Container,
   Grid,
@@ -19,26 +19,30 @@ interface IGalleryItemProps {
 
 const useStyles = makeStyles((theme: Theme) => ({
   wrap: {
-    height: '90vh',
     marginTop: 100,
     marginBottom: 200,
-    padding: 20,
     color: theme.palette.primary.contrastText,
+  },
+  container: {
+    maxWidth: '70vw',
   },
   font: {
     fontFamily: "'Caveat', cursive",
     color: theme.palette.secondary.main,
   },
   gallery: {
+    marginTop: 20,
     '& .image-gallery-icon:hover': { color: theme.palette.secondary.light },
     '& .image-gallery-thumbnail.active, .image-gallery-thumbnail:hover, .image-gallery-thumbnail:focus': {
       borderColor: theme.palette.secondary.light,
     },
-    '& .image-gallery-image': {
-      width: '100%',
-      height: 'calc(90vh - 80px)',
+    '& .image-gallery-slide, & .image-gallery-image': {
+      height: 'calc(85vh - 80px)',
       objectFit: 'cover',
     },
+    '& .image-gallery-description': {
+      background: 'rgba(0, 0, 0, 0.7)',
+    }
   },
 }));
 
@@ -47,16 +51,38 @@ const AttractionsGallery: React.FunctionComponent<IGalleryProps> = ({
 }) => {
   const classes = useStyles();
   const images = places.map((item: IGalleryItemProps) => ({
-    originalTitle: item.name,
-    original: `${item.photoUrl}`,
-    thumbnail: `${item.photoUrl}`,
+    name: item.name,
+    original: item.photoUrl,
+    thumbnail: item.photoUrl,
     description: item.description,
   }));
 
+  const [currentSlideId, setCurrentSlideId] = useState<number>(0);
+  const AttractionName = () => {
+    return (
+      <Typography component="span" className={classes.font} variant="h2">
+        {places?.[currentSlideId]?.name}
+      </Typography>
+    );
+  };
+
+  const setIndex = (id: number) => {
+    setCurrentSlideId(id);
+  };
+
   return (
-    <Grid className={classes.wrap} container alignItems="flex-end">
-      <Container>
-        <ImageGallery items={images} additionalClass={classes.gallery} />
+    <Grid className={classes.wrap} container>
+      <Container className={classes.container}>
+        <Typography align="center" color="textPrimary" variant="h2">
+          Country attractions
+        </Typography>
+        <AttractionName />
+        <ImageGallery
+          items={images}
+          thumbnailPosition="bottom"
+          additionalClass={classes.gallery}
+          onSlide={setIndex}
+        />
       </Container>
     </Grid>
   );
